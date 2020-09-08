06ebd178d6aef38e6c74321ffeb3cfad671b85aa
ainosato <56681766+ainosato@users.noreply.github.com>
2020-09-08 21:02:02 +0900

    Add files via upload

---
+++ Casting.java
@@ -0,0 +1,17 @@
+
+public class Casting {
+
+	public static void main(String[] args) {
+		double a = 1.1;
+		double b = 1;
+		System.out.println(b);
+		
+		double c = 1.1;
+		int e = (int) 1.1;
+		System.out.println(e);
+		
+		String f = Integer.toString(1);
+		System.out.println(f.getClass());
+	}
+
+}
